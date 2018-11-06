7323061f6d2b1c9e315bd65ff42269836edc1549
Wilbert Westerlo <1713388westerlo@zuyd.nl>
2018-11-06 12:19:18 +0100

    added models and controllers

---
+++ PlanningSystem/PlanningSystem/Controllers/UnavailabilityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanningSystem.Controllers
+{
+    public class UnavailabilityController : Controller
+    {
+        // GET: Unavailability
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
